235f447374f60a030c606fc53f4ece202d47b854
Eric W. Biederman <ebiederm@xmission.com>
2010-03-02 14:51:53 -0800

    pidns: Use task_active_pid_ns where appropriate
    The expressions tsk->nsproxy->pid_ns and task_active_pid_ns
    aka ns_of_pid(task_pid(tsk)) should have the same number of
    cache line misses with the practical difference that
    ns_of_pid(task_pid(tsk)) is released later in a processes life.
    
    Furthermore by using task_active_pid_ns it becomes trivial
    to write an unshare implementation for the the pid namespace.
    
    So I have used task_active_pid_ns everywhere I can.
    
    In fork since the pid has not yet been attached to the
    process I use ns_of_pid, to achieve the same effect.
    
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>
    
    Conflicts:
    	arch/um/drivers/mconsole_kern.c
    	drivers/staging/android/binder.c

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -34,6 +34,7 @@
 #include <linux/vmalloc.h>
 #include <linux/slab.h>
 #include <linux/security.h>
+#include <linux/pid_namespace.h>
 
 #include "binder.h"
 
@@ -2474,7 +2475,7 @@ retry:
 		if (t->from) {
 			struct task_struct *sender = t->from->proc->tsk;
 			tr.sender_pid = task_tgid_nr_ns(sender,
-							current->nsproxy->pid_ns);
+							task_active_pid_ns(current));
 		} else {
 			tr.sender_pid = 0;
 		}
