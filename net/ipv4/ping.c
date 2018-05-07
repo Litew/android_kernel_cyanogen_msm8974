48f71dd29b496ce030f0c411e05901740d8762af
litew <litew@tradiz.org>
2018-05-07 22:50:13 +0300

    Revert "net: ipv4: current group_info should be put after using."
    This reverts commit 62e06b89789022315d0e3070929be0854dd9d780.

--- net/ipv4/ping.c
+++ net/ipv4/ping.c
@@ -251,9 +251,8 @@ int ping_init_sock(struct sock *sk)
 	struct net *net = sock_net(sk);
 	gid_t group = current_egid();
 	gid_t range[2];
-	struct group_info *group_info;
-	int i, j, count  ;
-        int ret = 0;
+	struct group_info *group_info = get_current_groups();
+	int i, j, count = group_info->ngroups;
 
 	if (sk->sk_family == AF_INET6)
                 inet6_sk(sk)->ipv6only = 1;
@@ -262,25 +261,19 @@ int ping_init_sock(struct sock *sk)
 	if (range[0] <= group && group <= range[1])
 		return 0;
 
-	group_info = get_current_groups();
-	count = group_info->ngroups;
 	for (i = 0; i < group_info->nblocks; i++) {
 		int cp_count = min_t(int, NGROUPS_PER_BLOCK, count);
 
 		for (j = 0; j < cp_count; j++) {
 			group = group_info->blocks[i][j];
 			if (range[0] <= group && group <= range[1])
-				goto out_release_group;
+				return 0;
 		}
 
 		count -= cp_count;
 	}
 
-	ret = -EACCES;
-
-out_release_group:
-	put_group_info(group_info);
-	return ret;
+	return -EACCES;
 }
 EXPORT_SYMBOL_GPL(ping_init_sock);
 
